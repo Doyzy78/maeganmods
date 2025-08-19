b2cc9dc31bc15a642b72f42087f9c88129575dbf
Doyzy78 <162144547+Doyzy78@users.noreply.github.com>
2025-08-19 23:35:28 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const appData = [
+  {
+    id: "GG TOOLS",
+    name: "GG TOOLS Pro",
+    version: "2.0",
+    size: "15 MB",
+    icon: "images/icon.png",
+    apk: "apks/GG.lua", // Direct link
+    developer: "GG TOOLS / Developed by Joker Devs",
+    updated: "July 2025",
+    modFeatures: [
+      "✅ Value to Arm",
+      "✅ AutoUpdate Script",
+      "✅ SCRIPT Normal",
+      "✅ Menu Builder"
+    ]
+  }
+];
